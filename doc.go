f5370daf1da793844526c1dbff3a14a50b220c4e
徐胖 <youngxu@easyops.cn>
2020-05-19 15:01:46 +0800

    chore(): generate mocks

--- doc.go
+++ doc.go
@@ -1,1 +1,9 @@
 package mongo_driver_helper
+
+//go:generate mockgen -source pmongo/client.go          -destination pmongo/mock_pmongo/client.go
+//go:generate mockgen -source pmongo/collection.go      -destination pmongo/mock_pmongo/collection.go
+//go:generate mockgen -source pmongo/cursor.go          -destination pmongo/mock_pmongo/cursor.go
+//go:generate mockgen -source pmongo/database.go        -destination pmongo/mock_pmongo/database.go
+//go:generate mockgen -source pmongo/gridfs.go          -destination pmongo/mock_pmongo/gridfs.go
+//go:generate mockgen -source pmongo/index_view.go      -destination pmongo/mock_pmongo/index_view.go
+//go:generate mockgen -source pmongo/single_result.go   -destination pmongo/mock_pmongo/single_result.go
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/easyops-cn/mongo-driver-helper
 
 go 1.12
 
-require go.mongodb.org/mongo-driver v1.3.1
+require (
+	github.com/golang/mock v1.4.3
+	go.mongodb.org/mongo-driver v1.3.1
+)
